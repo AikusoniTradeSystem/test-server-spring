66c8662badd4afc3b2b957f4e905dcf52b8442c8
HoonKi Kim <31752803+aikusoni@users.noreply.github.com>
2024-08-24 12:43:28 +0900

    [feat/CORS:] * CrossOrigin 설정 추 가

---
+++ src/main/java/io/github/aikusonitradesystem/testserver/spring/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package io.github.aikusonitradesystem.testserver.spring.config;
+
+import io.github.aikusonitradesystem.authserver.session.properties.AuthServerProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("*"));
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
